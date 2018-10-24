d1ad9410b3a46c2b8e2892ab011b21aa7221de82
Jelle Alten <>
2018-10-24 12:32:18 +0200

    Add animating of color of Gauge.

--- GaugeKit/Gauge+Animations.swift
+++ GaugeKit/Gauge+Animations.swift
@@ -9,18 +9,24 @@
 import UIKit
 
 extension Gauge {
+
     public func animateRate(_ duration: TimeInterval, newValue: CGFloat, completion: @escaping (Bool) -> ()) -> Void {
+
+        animateRate(duration, newValue: newValue, newColor: nil, completion: completion)
+    }
+
+    public func animateRate(_ duration: TimeInterval, newValue: CGFloat, newColor: UIColor?, completion: @escaping (Bool) -> ()) -> Void {
         animationTimer.invalidate()
         
         let refreshRate: Double = 0.1
         let rateSpeed: CGFloat = CGFloat(refreshRate) * ((newValue - self.rate) / CGFloat(duration))
-        print(rateSpeed)
-        
+        //print(rateSpeed)
+
         animationTimer = Timer.scheduledTimer(
             timeInterval: refreshRate,
             target: self,
             selector: #selector(updateProgress),
-            userInfo: [newValue, rateSpeed],
+            userInfo: [newValue, ( newColor ?? startColor ), rateSpeed],
             repeats: true
         )
         
@@ -29,20 +35,26 @@ extension Gauge {
         animationCompletionBlock = completion
     }
     
-    func updateProgress(_ timer: Timer) -> Void {
-        let userInfo = timer.userInfo as! [CGFloat]
-        guard let newValue: CGFloat = userInfo.first else {
+    @objc func updateProgress(_ timer: Timer) -> Void {
+        let userInfo = timer.userInfo as! [Any]
+        guard let newValue = userInfo.first as? CGFloat else {
             print("GAUGE-KIT: Error, new value not defined...")
             return
         }
         
-        guard let rateSpeed: CGFloat = userInfo.last else {
+        guard let rateSpeed = userInfo.last as? CGFloat else {
             print("GAUGE-KIT: Error, rate speed could not be defined...")
             return
         }
         
-        self.rate += rateSpeed
+        guard let newColor = userInfo[1] as? UIColor else {
+            print("GAUGE-KIT: Error, new color not defined...")
+            return
+        }
         
+        self.rate += rateSpeed
+        self.startColor = newColor
+
         if rateSpeed < 0 {
             if self.rate <= newValue {
                 self.rate = newValue
